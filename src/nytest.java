26ea35d9eb2295a561ffc426d83e8ac78df261be
sival19 <sival19@student.sdu.dk>
2020-09-23 09:41:40 +0200

    test igen

--- src/nytest.java
+++ src/nytest.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class nytest {
+public class nytest {
+    public static void main(String[] args) {
+        int[][] hej =new int[][] {
+            {1,2,3,4,5,6,7,8,9},
+            {9,8,7,6,5,4,3,2,1}
+        };
+        //ny test her
+    }
 }
